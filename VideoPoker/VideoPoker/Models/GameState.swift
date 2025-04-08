a877974747ba84d9bb0715fdda6177f7458aa00d
AD <silentflute.ad@gmail.com>
2025-04-08 10:39:14 +0100

    Adding simple enum game state and rename file

---
+++ VideoPoker/VideoPoker/Models/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  VideoPoker
+//
+//  Created by Amarjit on 08/04/2025.
+//
+
+enum GameState {
+    case idle
+    case playerTurn
+    case dealerTurn
+    case evaluating
+    case gameOver
+}
